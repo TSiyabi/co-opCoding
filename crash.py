f008cfc714fd9f828ce5df0b62c5db014c9821ce
SuliCodeX <suleiman201804013@gmail.com>
2025-04-22 10:45:12 +0400

    adding a warning message | "DON'T RUN THE CODE"

--- crash.py
+++ crash.py
@@ -1,5 +1,7 @@
 import itertools
 
+print("WARNING, this code will definetely crash your laptop")
+
 for a,b,c in itertools.product(itertools.count(0),xrange(0,10),xrange(0,10)):
 	print(a)
 	print(b)
