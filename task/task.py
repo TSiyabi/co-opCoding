c764d466ad62e8409374fa13c17dc1b278517041
TSiyabi <tariqalsiyabi0077@gmail.com>
2025-04-23 15:08:37 +0400

    fixed it maybe

--- task/task.py
+++ task/task.py
@@ -1,44 +1,24 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-status = input("single or married")
-print("your status is" + status)
-=======
-=======
-country = input("what is your country of origin? ")
-print("your country of origin is " + country)
->>>>>>> 07c72033d2a598dff578c82adbf00eff0b1e1d58
-=======
-skills = input("Kindly list your key skills (separated by commas): ")
-
-# Process the input
-skill_list = [skill.strip() for skill in skills.split(',')]
-
-print("\nThank you for sharing your skills.")
->>>>>>> 6c7e0b1708b6d04b57f7b6a0488333f85419f9f2
-print("hello world")
-x = input("give me the password please\n3 chances before the most wanted hitman shoots you in the head\nand replaces you with a skin walker\ngood luck:")
-print(x)
+import datetime
 name = input("What's your name? ")
 age = int(input("How old are you? "))
-
-import datetime
-
+country = input("what is your country of origin? ")
+status = input("single or married")
+address = input("Enter ur address: ")
+skills = input("Kindly list your key skills (separated by commas): ")
+skill_list = [skill.strip() for skill in skills.split(',')]
 current_year = datetime.datetime.now().year
 year_hundred = current_year + (100 - age)
+password = input("give me the password please\n3 chances before the most wanted hitman shoots you in the head\nand replaces you with a skin walker\ngood luck:")
 
+
+print(address)
+print(x)
 print(f"Hello, {name}!")
 print(f"You will turn 100 years old in the year {year_hundred}.")
-print("Have a great day!")
-
-a = input("Enter ur address: ")
-print(a)
-
+print("\nThank you for sharing your skills.")
+print("hello world Have a great day!")
 print("OOOO")
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 93c71a30ec6ef3aedfe02fc7d7d19e6564daa428
-=======
-=======
->>>>>>> 07c72033d2a598dff578c82adbf00eff0b1e1d58
->>>>>>> 6c7e0b1708b6d04b57f7b6a0488333f85419f9f2
+print("your status is" + status)
+print("OOOO")
+print("your country of origin is " + country)
+print(password)
