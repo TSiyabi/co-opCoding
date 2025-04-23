c55e6c0a9164c2a4c79a0dff5131f5c36e5591f8
Sulaiman <Suleiman201804013@gmail.com>
2025-04-23 15:03:35 +0400

    fixed it

--- task/task.py
+++ task/task.py
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 status = input("single or married")
 print("your status is" + status)
-=======
-=======
 country = input("what is your country of origin? ")
 print("your country of origin is " + country)
->>>>>>> 07c72033d2a598dff578c82adbf00eff0b1e1d58
-=======
 skills = input("Kindly list your key skills (separated by commas): ")
 
 # Process the input
 skill_list = [skill.strip() for skill in skills.split(',')]
 
 print("\nThank you for sharing your skills.")
->>>>>>> 6c7e0b1708b6d04b57f7b6a0488333f85419f9f2
->>>>>>> 2ec48c50b01f6a8224a7deaf2485fda0d6341ced
 print("hello world")
 x = input("give me the password please\n3 chances before the most wanted hitman shoots you in the head\nand replaces you with a skin walker\ngood luck:")
 print(x)
@@ -37,15 +26,5 @@ print("Have a great day!")
 a = input("Enter ur address: ")
 print(a)
 
-<<<<<<< HEAD
 print("OOOOOOOOOOOOOOO")
-=======
 print("OOOO")
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 93c71a30ec6ef3aedfe02fc7d7d19e6564daa428
-=======
-=======
->>>>>>> 07c72033d2a598dff578c82adbf00eff0b1e1d58
->>>>>>> 6c7e0b1708b6d04b57f7b6a0488333f85419f9f2
->>>>>>> 2ec48c50b01f6a8224a7deaf2485fda0d6341ced
