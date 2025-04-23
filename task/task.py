d041fd2498f1cc45ffdf3e6867c56abb8b10ad88
Aiman-Amin <aimanamin101@gmail.com>
2025-04-23 14:53:38 +0400

    new update

--- task/task.py
+++ task/task.py
@@ -1,1 +1,6 @@
-print("hello world")
+skills = input("Kindly list your key skills (separated by commas): ")
+
+# Process the input
+skill_list = [skill.strip() for skill in skills.split(',')]
+
+print("\nThank you for sharing your skills.")
