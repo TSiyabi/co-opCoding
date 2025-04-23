205fc69809ed5a189c3d7e23aa583e5e75015851
MuhannadAlwahaibi <alwahaibi613@gmail.com>
2025-04-23 14:43:54 +0400

    anything

--- task/task.py
+++ task/task.py
@@ -9,3 +9,6 @@ year_hundred = current_year + (100 - age)
 print(f"Hello, {name}!")
 print(f"You will turn 100 years old in the year {year_hundred}.")
 print("Have a great day!")
+
+a = input("Enter ur address: ")
+print(a)
