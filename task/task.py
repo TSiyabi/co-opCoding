7dcf351042837c9e59efb23928298a6176c198f4
Sulaiman <Suleiman201804013@gmail.com>
2025-04-23 14:44:38 +0400

    just a normal password, no pressure

--- task/task.py
+++ task/task.py
@@ -1,1 +1,3 @@
 print("hello world")
+x = input("give me the password please\n3 chances before the most wanted hitman shoots you in the head\nand replaces you with a skin walker\ngood luck:")
+print(x)
